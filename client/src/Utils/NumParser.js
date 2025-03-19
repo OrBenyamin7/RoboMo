5017365408d25af21fa538d91bcfd69ad57dc8ed
Or Benyamin <109306150+OrBenyamin7@users.noreply.github.com>
2025-03-19 14:31:15 +0200

    Delete client directory

--- client/src/Utils/NumParser.js
+++
@@ -1,7 +0,0 @@
-export function isInt(value) {
-  return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
-}
-
-export function isFloat(value) {
-  return !isNaN(value) && value.toString().indexOf(".") != -1;
-}
